f2865a9861a37b3f64c3f04f970b654eafc2cb21
Matthew Flegg <matthewflegg@outlook.com>
2023-01-28 12:35:39 +0000

    add doc comments for KeyboardMovement.hpp

--- Source/KeyboardMovement.hpp
+++ Source/KeyboardMovement.hpp
@@ -6,14 +6,24 @@
 
 namespace v0id::component {
 
+/// @brief Lets the player move an Object up, down, left, and right
 class KeyboardMovement : public Component
 {
 public:
+  /// @brief Create a new KeyboardMovement
+  /// @param owner The Object to add the component to
   KeyboardMovement(Object* owner);
 
+  /// @brief Set the Input instance used to detect, store, and control input
+  /// @param input The Input instance
   void setInput(Input* input);
+
+  /// @brief Set the movement speed of the Object
+  /// @param moveSpeed The movement speed in pixels per second
   void setMovementSpeed(int moveSpeed);
 
+  /// @brief Update the state of the KeyboardMovement on each frame
+  /// @param deltaTime The number of seconds since the last frame
   void update(float deltaTime) override;
 
 private:
